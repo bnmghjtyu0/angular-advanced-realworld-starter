0faa0e94d71f567e2ab136bf7129c4bd7a000eb8
Richard.Liao <richard.liao@tpisoftware.com>
2022-07-31 12:00:06 +0800

    chore: single angular component module

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,11 +2,13 @@ import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout/layout.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderModule } from '../shared/header/header.module';
 
 @NgModule({
-  declarations: [LayoutComponent],
-  imports: [CommonModule, RouterModule],
-  exports: [],
+  declarations: [LayoutComponent, FooterComponent],
+  imports: [CommonModule, RouterModule, HeaderModule],
+  exports: [FooterComponent],
   providers: [],
 })
 export class LayoutModule {}
