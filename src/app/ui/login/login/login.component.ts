c698aa9a770d6779d7b8127583c92659be665096
Richard.Liao <richard.liao@tpisoftware.com>
2022-07-31 14:45:14 +0800

    任務 04：在程式中動態切換路由

--- src/app/ui/login/login/login.component.ts
+++ src/app/ui/login/login/login.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/login.service';
 
+
+/**登入頁 */
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  /**建構子 */
+  constructor(private router: Router, private loginService: LoginService) {}
 
-  ngOnInit(): void {
+  /**登入 */
+  login(): void {
+    this.loginService
+      .login({ email: 'demo@miniasp.com', password: '123456' })
+      .subscribe({
+        next: (result) => {
+          localStorage.setItem('jwtToken', result.user.token);
+          this.router.navigate(['/']);
+        },
+      });
   }
-
 }
