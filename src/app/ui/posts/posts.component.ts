b1df64691335446ff2aab2c7dd329a8148baadd0
Richard.Liao <richard.liao@tpisoftware.com>
2022-07-31 14:23:21 +0800

    fix: Mike 老師實作後的調整。shareReplay(1) Refresh 按鈕

--- src/app/ui/posts/posts.component.ts
+++ src/app/ui/posts/posts.component.ts
@@ -1,5 +1,5 @@
-import { map, Observable } from 'rxjs';
-import { shareReplay } from 'rxjs/operators';
+import { BehaviorSubject, map, Observable } from 'rxjs';
+import { shareReplay, switchMap } from 'rxjs/operators';
 import { Component } from '@angular/core';
 import { PostService } from 'src/app/post.service';
 import { Article } from 'src/app/interfaces/article';
@@ -14,12 +14,19 @@ import { Article } from 'src/app/interfaces/article';
 })
 export class PostsComponent {
   displayArticles = false;
+  refresh$ = new BehaviorSubject(null);
   /**畫面有用到，才會取得 API */
-  articles$: Observable<Article[]> = this.postService.getArticles().pipe(
+  articles$: Observable<Article[]> = this.refresh$.pipe(
+    switchMap(() => this.postService.getArticles()),
     map((result) => result.articles),
     //cache the result
     shareReplay(1)
   );
 
+
   constructor(private postService: PostService) {}
+
+  refresh() {
+    this.refresh$.next(null);
+  }
 }
