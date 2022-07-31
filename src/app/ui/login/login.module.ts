065e97ca4904efc890459ddba264867bfc091c3a
Richard.Liao <richard.liao@tpisoftware.com>
2022-07-31 15:30:17 +0800

    任務 05：Template-driven Form 驗證表單資料

--- src/app/ui/login/login.module.ts
+++ src/app/ui/login/login.module.ts
@@ -2,11 +2,11 @@ import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, LoginRoutingModule, FormsModule],
+  imports: [CommonModule, LoginRoutingModule, FormsModule, ReactiveFormsModule],
   exports: [],
   providers: [],
 })
--- src/app/ui/login/login/login.component.ts
+++ src/app/ui/login/login/login.component.ts
@@ -1,5 +1,13 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  FormGroupDirective,
+  NgModel,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoginService } from 'src/app/login.service';
 
@@ -15,20 +23,42 @@ export class LoginComponent {
     password: '123456',
   };
 
+  form: FormGroup = this.fb.group({
+    email: this.fb.control('', {
+      validators: [Validators.required, Validators.email],
+    }),
+    password: this.fb.control('', {
+      validators: [Validators.required, Validators.min(4)],
+    }),
+  });
+
   /**建構子 */
-  constructor(private router: Router, private loginService: LoginService) {}
+  constructor(
+    private router: Router,
+    private loginService: LoginService,
+    private fb: FormBuilder
+  ) {}
 
   /**登入 */
   login(): void {
-    this.loginService.login(this.user).subscribe({
-      next: (result) => {
-        localStorage.setItem('token', result.user.token);
-        this.router.navigate(['/']);
-      },
-      error: (error: HttpErrorResponse) => {
-        alert(error.error.body);
-      },
-      complete: () => {},
-    });
+    console.log(this.form);
+    if (this.form.valid) {
+      this.loginService.login(this.user).subscribe({
+        next: (result) => {
+          localStorage.setItem('token', result.user.token);
+          this.router.navigate(['/']);
+        },
+        error: (error: HttpErrorResponse) => {
+          alert(error.error.body);
+        },
+        complete: () => {},
+      });
+    }
+  }
+  isInvalid(control: AbstractControl, formRef: FormGroupDirective) {
+    return control.invalid && (control.touched || formRef.submitted);
+  }
+  isValid(control: AbstractControl, formRef: FormGroupDirective) {
+    return control.valid && (control.touched || formRef.submitted);
   }
 }
